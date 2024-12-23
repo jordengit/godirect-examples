e97fbebffae5457b0c9e2471ebd6a672eda91474
jorden.wang <jorden.wang@gmail.com>
2024-12-24 01:38:01 +0800

    2024122402_OK

--- python/plateclient.py
+++ python/plateclient.py
@@ -4,6 +4,9 @@ from gdx import gdx
 from time import sleep
 import time
 import requests
+import os
+import time
+from multiprocessing import Process
 
 # setup loggers
 logging.config.fileConfig('plateclient.logconfig', disable_existing_loggers=False)
@@ -25,10 +28,11 @@ def pass_data_to_server(deviceid, data, cnt, ts):
     return
 def pass_buf_to_server(buf):
 
+    newbuf = {'data': buf}
     # Define the API endpoint
-    api_url = f"http://127.0.0.1:8000/addbuf"
+    api_url = f"http://127.0.0.1:8000/addbuf/"
     # Make a GET request to the API
-    response = requests.post(api_url, json = buf)
+    response = requests.post(api_url, json = newbuf)
 
     # Check if the request was successful
     if response.status_code != 200:
@@ -38,35 +42,41 @@ def pass_buf_to_server(buf):
 def current_milli_time():
     return round(time.time() * 1000)
 
-gdx = gdx.gdx()
-gdx.open(connection='ble',device_to_open='GDX-FP 1J1000T6')
-# sensor type: Force
-gdx.select_sensors(1)
-# sample rate: 1ms
-gdx.start(1)
-column_headers= gdx.enabled_sensor_info()   # returns a string with sensor description and units
-logger.info('\n')
-logger.info(column_headers)
+def func(*buf):
+    pass_buf_to_server(buf)
 
-databuffer = []
-count = 0
-try:
-    while True:
-        measurements = gdx.read()
-        if measurements == None:
-            break
-        count = count + 1
-        ts = current_milli_time()
-        # logger.info(f"count:{count} ts:{ts} measurements:{measurements[0]}")
-        newdata = ["deviceid",count,measurements[0],ts/1000]
-        databuffer.append(newdata)
-        if (count > 1 && count % 1000 == 0)
-            pass_buf_to_server(databuffer)
-            databuffer = []
-except KeyboardInterrupt:
-    pass
+if __name__ == "__main__":
+    gdx = gdx.gdx()
+    gdx.open(connection='ble',device_to_open='GDX-FP 1J1000T6')
+    # sensor type: Force
+    gdx.select_sensors(1)
+    # sample rate: 1ms
+    gdx.start(1)
+    column_headers= gdx.enabled_sensor_info()   # returns a string with sensor description and units
+    logger.info('\n')
+    logger.info(column_headers)
 
-gdx.stop()
-gdx.close()
+    databuffer = []
+    count = 0
+    try:
+        while True:
+            measurements = gdx.read()
+            if measurements == None:
+                break
+            count = count + 1
+            ts = current_milli_time()
+            # logger.info(f"count:{count} ts:{ts} measurements:{measurements[0]}")
+            newdata = ["deviceid",count,measurements[0],ts]
+            databuffer.append(newdata)
+            if (count > 1 and count % 1000 == 0):
+                p = Process(target=func, args=(databuffer))
+                p.start()
+                # pass_buf_to_server(databuffer)
+                databuffer = []
+    except KeyboardInterrupt:
+        pass
+
+    gdx.stop()
+    gdx.close()
 
 # input("Press Enter to exit...")
--- python/venv.bat
+++ python/venv.bat
@@ -1,4 +1,4 @@
 set CONDA_ROOT_PREFIX=D:\miniforge3
-set INSTALL_ENV_DIR=%CONDA_ROOT_PREFIX%\envs\veriner
+set INSTALL_ENV_DIR=D:\miniforge3\envs\veniner
 call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%" || ( echo. && echo ERROR: Miniconda hook not found. && goto end )
 cmd
